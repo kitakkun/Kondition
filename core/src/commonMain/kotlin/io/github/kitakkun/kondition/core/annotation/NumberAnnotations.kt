6f5c87482caf1235ab529201ab7f875b0bc9a249
kitakkun <48154936+kitakkun@users.noreply.github.com>
2024-10-09 23:37:31 +0900

    Allow to annotate local variables

--- core/src/commonMain/kotlin/io/github/kitakkun/kondition/core/annotation/NumberAnnotations.kt
+++ core/src/commonMain/kotlin/io/github/kitakkun/kondition/core/annotation/NumberAnnotations.kt
@@ -2,19 +2,19 @@
 
 package io.github.kitakkun.kondition.core.annotation
 
-@Target(AnnotationTarget.VALUE_PARAMETER)
+@Target(AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.LOCAL_VARIABLE)
 annotation class NonZero
 
-@Target(AnnotationTarget.VALUE_PARAMETER)
+@Target(AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.LOCAL_VARIABLE)
 annotation class Positive
 
-@Target(AnnotationTarget.VALUE_PARAMETER)
+@Target(AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.LOCAL_VARIABLE)
 annotation class Negative
 
-@Target(AnnotationTarget.VALUE_PARAMETER)
+@Target(AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.LOCAL_VARIABLE)
 annotation class NonPositive
 
-@Target(AnnotationTarget.VALUE_PARAMETER)
+@Target(AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.LOCAL_VARIABLE)
 annotation class NonNegative
 
 enum class RangeRule {
@@ -24,49 +24,49 @@ enum class RangeRule {
     InclusiveExclusive,
 }
 
-@Target(AnnotationTarget.VALUE_PARAMETER)
+@Target(AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.LOCAL_VARIABLE)
 annotation class RangedInt(
     val start: Int,
     val end: Int,
     val rule: RangeRule = RangeRule.InclusiveInclusive,
 )
 
-@Target(AnnotationTarget.VALUE_PARAMETER)
+@Target(AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.LOCAL_VARIABLE)
 annotation class RangedLong(
     val start: Long,
     val end: Long,
     val rule: RangeRule = RangeRule.InclusiveInclusive,
 )
 
-@Target(AnnotationTarget.VALUE_PARAMETER)
+@Target(AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.LOCAL_VARIABLE)
 annotation class RangedShort(
     val start: Short,
     val end: Short,
     val rule: RangeRule = RangeRule.InclusiveInclusive,
 )
 
-@Target(AnnotationTarget.VALUE_PARAMETER)
+@Target(AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.LOCAL_VARIABLE)
 annotation class RangedByte(
     val start: Byte,
     val end: Byte,
     val rule: RangeRule = RangeRule.InclusiveInclusive,
 )
 
-@Target(AnnotationTarget.VALUE_PARAMETER)
+@Target(AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.LOCAL_VARIABLE)
 annotation class RangedDouble(
     val start: Double,
     val end: Double,
     val rule: RangeRule = RangeRule.InclusiveInclusive,
 )
 
-@Target(AnnotationTarget.VALUE_PARAMETER)
+@Target(AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.LOCAL_VARIABLE)
 annotation class GreaterThan(val threshold: Double)
 
-@Target(AnnotationTarget.VALUE_PARAMETER)
+@Target(AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.LOCAL_VARIABLE)
 annotation class GreaterThanOrEquals(val threshold: Double)
 
-@Target(AnnotationTarget.VALUE_PARAMETER)
+@Target(AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.LOCAL_VARIABLE)
 annotation class LessThan(val threshold: Double)
 
-@Target(AnnotationTarget.VALUE_PARAMETER)
+@Target(AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.LOCAL_VARIABLE)
 annotation class LessThanOrEquals(val threshold: Double)
--- core/src/commonMain/kotlin/io/github/kitakkun/kondition/core/annotation/StringAnnotations.kt
+++ core/src/commonMain/kotlin/io/github/kitakkun/kondition/core/annotation/StringAnnotations.kt
@@ -2,26 +2,26 @@
 
 package io.github.kitakkun.kondition.core.annotation
 
-@Target(AnnotationTarget.VALUE_PARAMETER)
+@Target(AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.LOCAL_VARIABLE)
 annotation class NonEmpty
 
-@Target(AnnotationTarget.VALUE_PARAMETER)
+@Target(AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.LOCAL_VARIABLE)
 annotation class NonBlank
 
-@Target(AnnotationTarget.VALUE_PARAMETER)
+@Target(AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.LOCAL_VARIABLE)
 annotation class MatchRegex(val pattern: String)
 
-@Target(AnnotationTarget.VALUE_PARAMETER)
+@Target(AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.LOCAL_VARIABLE)
 annotation class Alphabetic
 
-@Target(AnnotationTarget.VALUE_PARAMETER)
+@Target(AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.LOCAL_VARIABLE)
 annotation class Numeric
 
-@Target(AnnotationTarget.VALUE_PARAMETER)
+@Target(AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.LOCAL_VARIABLE)
 annotation class Length(val length: Int)
 
-@Target(AnnotationTarget.VALUE_PARAMETER)
+@Target(AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.LOCAL_VARIABLE)
 annotation class MinLength(val length: Int)
 
-@Target(AnnotationTarget.VALUE_PARAMETER)
+@Target(AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.LOCAL_VARIABLE)
 annotation class MaxLength(val length: Int)
