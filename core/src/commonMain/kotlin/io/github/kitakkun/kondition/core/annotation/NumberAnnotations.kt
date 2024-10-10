7ea8d5cbbc16a38fe91075c2bb7fb3b5d86fd652
kitakkun <48154936+kitakkun@users.noreply.github.com>
2024-10-11 00:24:34 +0900

    Add decimal and non-decimal versions of annotations

--- core/src/commonMain/kotlin/io/github/kitakkun/kondition/core/annotation/NumberAnnotations.kt
+++ core/src/commonMain/kotlin/io/github/kitakkun/kondition/core/annotation/NumberAnnotations.kt
@@ -44,14 +44,32 @@ annotation class RangedDecimal(
     val rule: RangeRule = RangeRule.InclusiveInclusive,
 )
 
+/**
+ * the argument type is [Long], but applicable for [Int], [Short], [Byte], or other numbers.
+ */
 @Target(AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.LOCAL_VARIABLE)
-annotation class GreaterThan(val threshold: Double)
+annotation class GreaterThan(val threshold: Long)
+
+/**
+ * the argument type is [Double], but applicable for [Float] or other numbers.
+ */
+@Target(AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.LOCAL_VARIABLE)
+annotation class GreaterThanDecimal(val threshold: Double)
 
 @Target(AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.LOCAL_VARIABLE)
-annotation class GreaterThanOrEquals(val threshold: Double)
+annotation class GreaterThanOrEquals(val threshold: Long)
 
 @Target(AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.LOCAL_VARIABLE)
-annotation class LessThan(val threshold: Double)
+annotation class GreaterThanOrEqualsDecimal(val threshold: Double)
+
+@Target(AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.LOCAL_VARIABLE)
+annotation class LessThan(val threshold: Long)
+
+@Target(AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.LOCAL_VARIABLE)
+annotation class LessThanDecimal(val threshold: Double)
 
 @Target(AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.LOCAL_VARIABLE)
 annotation class LessThanOrEquals(val threshold: Double)
+
+@Target(AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.LOCAL_VARIABLE)
+annotation class LessThanOrEqualsDecimal(val threshold: Long)
