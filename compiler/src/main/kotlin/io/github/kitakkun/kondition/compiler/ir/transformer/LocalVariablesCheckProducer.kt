2eaf4d8bb9191be8d960dae49edd7b3973cfae80
kitakkun <48154936+kitakkun@users.noreply.github.com>
2024-10-17 20:42:22 +0900

    Fix potential compile error due to local variables

--- compiler/src/main/kotlin/io/github/kitakkun/kondition/compiler/ir/transformer/LocalVariablesCheckProducer.kt
+++ compiler/src/main/kotlin/io/github/kitakkun/kondition/compiler/ir/transformer/LocalVariablesCheckProducer.kt
@@ -2,72 +2,66 @@ package io.github.kitakkun.kondition.compiler.ir.transformer
 
 import io.github.kitakkun.kondition.compiler.ir.KonditionIrContext
 import io.github.kitakkun.kondition.compiler.ir.statement.StatementsProducer
-import io.github.kitakkun.kondition.compiler.ir.visitor.LocalVariableCollector
 import org.jetbrains.kotlin.backend.common.lower.createIrBuilder
 import org.jetbrains.kotlin.backend.common.lower.irComposite
 import org.jetbrains.kotlin.ir.IrStatement
+import org.jetbrains.kotlin.ir.builders.irComposite
 import org.jetbrains.kotlin.ir.declarations.IrFunction
 import org.jetbrains.kotlin.ir.declarations.IrVariable
 import org.jetbrains.kotlin.ir.expressions.IrExpression
 import org.jetbrains.kotlin.ir.expressions.IrSetValue
 import org.jetbrains.kotlin.ir.symbols.UnsafeDuringIrConstructionAPI
+import org.jetbrains.kotlin.ir.util.isLocal
 import org.jetbrains.kotlin.ir.visitors.IrElementTransformerVoid
-import org.jetbrains.kotlin.ir.visitors.transformChildrenVoid
 
 class LocalVariablesCheckProducer(
     private val irContext: KonditionIrContext,
     private val statementsProducer: StatementsProducer,
 ) : IrElementTransformerVoid() {
-    private val localVariableCollector = LocalVariableCollector()
+    override fun visitVariable(declaration: IrVariable): IrStatement {
+        if (!declaration.isLocal) return super.visitVariable(declaration)
+
+        val parent = declaration.parent as? IrFunction ?: return super.visitVariable(declaration)
+
+        val irBuilder = irContext.irBuiltIns.createIrBuilder(declaration.symbol)
+
+        val checkStatements = with(statementsProducer) {
+            irBuilder.produce(
+                irContext = irContext,
+                value = declaration,
+                parentDeclaration = parent,
+            )
+        }
+
+        if (checkStatements.isEmpty()) return super.visitVariable(declaration)
+
+        return irBuilder.irComposite(declaration.startOffset, declaration.endOffset) {
+            +declaration
+            +checkStatements
+        }
+    }
 
     @OptIn(UnsafeDuringIrConstructionAPI::class)
-    override fun visitFunction(declaration: IrFunction): IrStatement {
-        val localVariables = localVariableCollector.collectFunctionLocalVariables(declaration)
+    override fun visitSetValue(expression: IrSetValue): IrExpression {
+        if (!expression.symbol.owner.isLocal) return super.visitSetValue(expression)
 
-        val body = declaration.body ?: return super.visitFunction(declaration)
+        val parent = expression.symbol.owner.parent as? IrFunction ?: return super.visitSetValue(expression)
 
-        // to reference function declaration inside child transformer...
-        val function = declaration
+        val irBuilder = irContext.irBuiltIns.createIrBuilder(expression.symbol)
 
-        body.transformChildrenVoid(
-            object : IrElementTransformerVoid() {
-                override fun visitSetValue(expression: IrSetValue): IrExpression {
-                    val correspondingLocalVariable = localVariables.find { it.symbol == expression.symbol } ?: return super.visitSetValue(expression)
+        val checkStatements = with(statementsProducer) {
+            irBuilder.produce(
+                irContext = irContext,
+                value = expression.symbol.owner,
+                parentDeclaration = parent,
+            )
+        }
 
-                    return with(irContext.irBuiltIns.createIrBuilder(expression.symbol)) {
-                        irComposite(expression) {
-                            +expression
-                            +with(statementsProducer) {
-                                produce(
-                                    irContext = irContext,
-                                    value = correspondingLocalVariable,
-                                    parentDeclaration = declaration,
-                                )
-                            }
-                        }
-                    }
-                }
+        if (checkStatements.isEmpty()) return super.visitSetValue(expression)
 
-                override fun visitVariable(declaration: IrVariable): IrStatement {
-                    val correspondingLocalVariable = localVariables.find { it.symbol == declaration.symbol } ?: return super.visitVariable(declaration)
-                    val initializer = correspondingLocalVariable.initializer ?: return super.visitVariable(declaration)
-
-                    return with(irContext.irBuiltIns.createIrBuilder(correspondingLocalVariable.symbol)) {
-                        irComposite(initializer) {
-                            +declaration
-                            +with(statementsProducer) {
-                                produce(
-                                    irContext = irContext,
-                                    value = correspondingLocalVariable,
-                                    parentDeclaration = function,
-                                )
-                            }
-                        }
-                    }
-                }
-            },
-        )
-
-        return super.visitFunction(declaration)
+        return irBuilder.irComposite(expression) {
+            +expression
+            +checkStatements
+        }
     }
 }
