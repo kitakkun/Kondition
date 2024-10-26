588036aa7db004e1be5f85cb2ccc7ed2265d7cb9
kitakkun <48154936+kitakkun@users.noreply.github.com>
2024-10-26 23:26:47 +0900

    Check invalid annotation usages by meta annotations

--- compiler/src/main/kotlin/io/github/kitakkun/kondition/compiler/fir/KonditionErrorMessages.kt
+++ compiler/src/main/kotlin/io/github/kitakkun/kondition/compiler/fir/KonditionErrorMessages.kt
@@ -8,6 +8,13 @@ object KonditionErrorMessages : BaseDiagnosticRendererFactory() {
     @Suppress("ktlint:standard:property-naming")
     override val MAP: KtDiagnosticFactoryToRendererMap = KtDiagnosticFactoryToRendererMap("kondition").apply {
         put(
+            KonditionErrors.KONDITION_ANNOTATION_USED_AGAINST_NON_APPLICABLE_TYPE,
+            "Annotation {0} is not applicable to the type {1}. Applicable types are: {2}",
+            rendererA = TO_STRING,
+            rendererB = TO_STRING,
+            rendererC = TO_STRING,
+        )
+        put(
             KonditionErrors.RETURN_IMPOSSIBLE_FOR_NON_UNIT_TYPE,
             "{0}: RETURN strategy can't be applied to the function with non-Unit return type.",
             rendererA = TO_STRING,
@@ -17,14 +24,6 @@ object KonditionErrorMessages : BaseDiagnosticRendererFactory() {
             "{0} is out of range of {1} type. It may cause unexpected behavior.",
             rendererA = TO_STRING,
             rendererB = TO_STRING,
-        )
-        put(
-            KonditionErrors.INVALID_NUMBER_ANNOTATION_USAGE,
-            "Invalid usage of Number annotation",
-        )
-        put(
-            KonditionErrors.INVALID_STRING_ANNOTATION_USAGE,
-            "Invalid usage of String annotation",
         )
     }
 }
